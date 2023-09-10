1aeac8a6ede25b0f29737d13c9bd9505104ad094
Max Carvajal <jcarvajal288@gmail.com>
2023-09-10 16:22:02 -0500

    Instantiate puyo pair

---
+++ scripts/puyo_pair.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _init():
+	var puyo1 = preload("res://puyo.tscn").instantiate()
+	var puyo2 = preload("res://puyo.tscn").instantiate()
+	puyo1.color = randi() % 5
+	puyo2.color = randi() % 5
+	puyo2.position.x += 32
+	add_child(puyo1)
+	add_child(puyo2)
