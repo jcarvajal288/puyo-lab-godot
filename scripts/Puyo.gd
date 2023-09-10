a4d81f68bed2cdbb5629fd97676b011c87c58a64
Max Carvajal <jcarvajal288@gmail.com>
2023-09-10 13:23:31 -0500

    Choose animation based on puyo color

--- scripts/Puyo.gd
+++ scripts/Puyo.gd
@@ -6,7 +6,16 @@ enum PuyoColor { RED, YELLOW, GREEN, BLUE, PURPLE}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.play()
+	if color == PuyoColor.RED:
+		$AnimatedSprite2D.play("red")
+	elif color == PuyoColor.YELLOW:
+		$AnimatedSprite2D.play("yellow")
+	elif color == PuyoColor.GREEN:
+		$AnimatedSprite2D.play("green")
+	elif color == PuyoColor.BLUE:
+		$AnimatedSprite2D.play("blue")
+	elif color == PuyoColor.PURPLE:
+		$AnimatedSprite2D.play("purple")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
