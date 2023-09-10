037d5b100dac79aa03087cca45da9e608a723b31
Max Carvajal <jcarvajal288@gmail.com>
2023-09-10 16:48:01 -0500

    Move PlayBorder into PlayField scene

---
+++ scripts/play_field.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+const STARTING_POSITION = Vector2(32, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var puyoPair = preload("res://puyo_pair.tscn").instantiate()
+	puyoPair.position = STARTING_POSITION
+	add_child(puyoPair)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
