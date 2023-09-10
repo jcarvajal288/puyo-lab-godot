ee2352846554a564e4725e2273ad3248c24ac60a
Max Carvajal <jcarvajal288@gmail.com>
2023-09-10 13:28:02 -0500

    Fix some too-fast animations

--- scripts/Puyo.gd
+++ scripts/Puyo.gd
@@ -1,21 +1,17 @@
 extends RigidBody2D
 
-enum PuyoColor { RED, YELLOW, GREEN, BLUE, PURPLE}
+enum PuyoColor { RED, YELLOW, GREEN, BLUE, PURPLE }
 
 @export var color: PuyoColor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if color == PuyoColor.RED:
-		$AnimatedSprite2D.play("red")
-	elif color == PuyoColor.YELLOW:
-		$AnimatedSprite2D.play("yellow")
-	elif color == PuyoColor.GREEN:
-		$AnimatedSprite2D.play("green")
-	elif color == PuyoColor.BLUE:
-		$AnimatedSprite2D.play("blue")
-	elif color == PuyoColor.PURPLE:
-		$AnimatedSprite2D.play("purple")
+	match color:
+		PuyoColor.RED: $AnimatedSprite2D.play("red")
+		PuyoColor.YELLOW: $AnimatedSprite2D.play("yellow")
+		PuyoColor.GREEN: $AnimatedSprite2D.play("green")
+		PuyoColor.BLUE: $AnimatedSprite2D.play("blue")
+		PuyoColor.PURPLE: $AnimatedSprite2D.play("purple")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
