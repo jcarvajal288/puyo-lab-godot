3116cb1dd983a84afb7076929edc66dc58b8b188
Max Carvajal <jcarvajal288@gmail.com>
2023-09-11 13:52:46 -0500

    Add left/right/down movement

--- scripts/play_field.gd
+++ scripts/play_field.gd
@@ -1,14 +1,22 @@
 extends Node2D
 
-const STARTING_POSITION = Vector2(32, 0)
+const TILE_SIZE = 32
+const STARTING_POSITION = Vector2(TILE_SIZE, 0)
+
+var puyoPair
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var puyoPair = preload("res://scenes/puyo_pair.tscn").instantiate()
+	puyoPair = preload("res://scenes/puyo_pair.tscn").instantiate()
 	puyoPair.position = STARTING_POSITION
 	add_child(puyoPair)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("move_right"):
+		puyoPair.position.x += TILE_SIZE
+	elif Input.is_action_just_pressed("move_left"):
+		puyoPair.position.x -= TILE_SIZE
+	elif Input.is_action_just_pressed("move_down"):
+		puyoPair.position.y += TILE_SIZE
